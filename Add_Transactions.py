1a0ee13b89c579840e16ecc72ccd1359bf063425
Gillian Vasoff <gillvasoff@gmail.com>
2025-08-25 07:40:20 -0400

    Refactoring global script
    Refactored a large global script into smaller functions. The body of the
    script is still global, but the logic executes within functions.

--- Add_Transactions.py
+++ Add_Transactions.py
@@ -4,35 +4,41 @@ import pandas as pd
 import configparser
 
 
-try:        
-    
+try:           
     config = configparser.ConfigParser()
-    config.read('Configurations/database_config.ini')
 
-    app_host_name = config['app_connection']['app_hostname']
-    app_database_name = config['app_connection']['app_database']
-    app_username = config['app_connection']['app_username']
-    app_password = config['app_connection']['app_pwd']
-    app_port = config['app_connection']['app_port']
-    app_table_name = config['app_connection']['app_table']
+    def get_df_from_csv():
+        config.read('Configurations/csv_config.ini')
+        csv_input_path = config['csv_files']['input_path']
 
-    # Open a connection to the PostgreSQL database
-    conn = psycopg2.connect(host=app_host_name,
-                            dbname=app_database_name,
-                            user=app_username,
-                            password=app_password,
-                            port=app_port)
+        # Open the csv in pandas
+        return pd.read_csv(csv_input_path, header=None)  
+    
+    def save_df_to_csv(df):
+        config.read('Configurations/csv_config.ini')
+        csv_processed_path = config['csv_files']['processed_path']
+        # Save the modified DataFrame to the original CSV file
+        df.to_csv(csv_processed_path, index=False, header=False)
 
-    cur = conn.cursor()
+    def get_csv_data():
+        csv_data: StringIO
+        config.read('Configurations/csv_config.ini')
+        csv_processed_path = config['csv_files']['processed_path']
+        with open(csv_processed_path, 'r') as f:
+            csv_data = f.read()
+        return StringIO(csv_data)
 
-    config.read('Configurations/csv_config.ini')
-    csv_input_path = config['csv_files']['input_path']
-    csv_processed_path = config['csv_files']['processed_path']
+    def get_formatted_df_from_csv():
+        df = get_df_from_csv()
+        df = format_columns(df)
+        return df
 
-    # Open the csv in pandas
-    df = pd.read_csv(csv_input_path, header=None)
-
-    print(df)
+    def copy_csv_to_table():
+        df = get_formatted_df_from_csv()
+        save_df_to_csv(df)
+        csv_data = get_csv_data()
+        # Copy the csv file to our PostgreSQL table
+        cur.copy_from(csv_data, app_table_name, sep=',', columns=('transaction_date', 'to_or_from', 'withdrawl', 'deposit','balance'))   
 
     def is_US_format(split_list):
         if len(split_list) != 3:
@@ -64,29 +70,34 @@ try:
 
     def replace_empty_col_with_zeros(x):
         if pd.isna(x):
-            return 0
-        return x
+            return 0.0
+        else:
+            return x
 
-    df.iloc[:,0] = df.iloc[:,0].apply(format_date_US_to_ISO)
-    df.iloc[:,2] = df.iloc[:,2].apply(replace_empty_col_with_zeros)
-    df.iloc[:,3] = df.iloc[:,3].apply(replace_empty_col_with_zeros)
-
-    # Save the modified DataFrame to the original CSV file
-    df.to_csv(csv_processed_path, index=False, header=False)
-
-    csv_data: StringIO
-
-    csv_path = csv_processed_path
-    with open(csv_path, 'r') as f:
-        csv_data = f.read()
-
-    csv = StringIO(csv_data)
+    def format_columns(df):
+        df.iloc[:,0] = df.iloc[:,0].apply(format_date_US_to_ISO)
+        df.iloc[:,2] = df.iloc[:,2].apply(replace_empty_col_with_zeros)
+        df.iloc[:,3] = df.iloc[:,3].apply(replace_empty_col_with_zeros)
+        return df
 
     config.read('Configurations/database_config.ini')
 
-    # Copy the csv file to our PostgreSQL table
-    cur.copy_from(csv, app_table_name, sep=',', columns=('transaction_date', 'to_or_from', 'withdrawl', 'deposit','balance'))
+    app_host_name = config['app_connection']['app_hostname']
+    app_database_name = config['app_connection']['app_database']
+    app_username = config['app_connection']['app_username']
+    app_password = config['app_connection']['app_pwd']
+    app_port = config['app_connection']['app_port']
+    app_table_name = config['app_connection']['app_table']
 
+    # Open a connection to the PostgreSQL database
+    conn = psycopg2.connect(host=app_host_name,
+                            dbname=app_database_name,
+                            user=app_username,
+                            password=app_password,
+                            port=app_port)
+
+    cur = conn.cursor()
+    copy_csv_to_table()
     conn.commit()
 
 # Catch errors and close connection
